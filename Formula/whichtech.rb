c26d04e9a46835dfca8686d5ed6396ac4edd0be9
Anas AMMOR <59310018+darks-society@users.noreply.github.com>
2024-06-08 02:07:12 +0100

    Create whichtech.rb

---
+++ Formula/whichtech.rb
@@ -0,0 +1,15 @@
+class Whichtech < Formula
+  desc "A CLI tool to detect the technology used in a project"
+  homepage "https://github.com/darks-society/whichtech"
+  url "https://github.com/darks-society/whichtech/archive/v1.0.0.tar.gz"
+  sha256 "86fd96d660d8f32b6f4d460ceb36da5f5161d974a6d24f66985ccb4be769d15e"
+  version "1.0.0"
+
+  def install
+    bin.install "whichtech"
+  end
+
+  test do
+    system "#{bin}/whichtech", "--version"
+  end
+end
